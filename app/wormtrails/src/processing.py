656bbeb3cf1a4f15b82aaaa4a36191445966a98b
C. Dante Ashih <50035608+Commentator-2@users.noreply.github.com>
2026-01-20 15:36:29 -0500

    Update processing.py
    Added useful comments and docstrings

--- app/wormtrails/src/processing.py
+++ app/wormtrails/src/processing.py
@@ -3,26 +3,52 @@ import numpy as np
 import math
 
 def correct_vignetting(video_array):
+    """
+    Corrects vignetting in a video array by normalizing each frame to have the same brightness as the average frame and dividing by a blur of the average frame.
+    This step can be skipped if initial scan is evenly lit and the lens used does not vignette.
+
+    Args:
+        video_array: 3D Numpy array containing the video frames, with time as axis 0.
+
+    Returns:
+        video_array: 3D Numpy array of 8 bit unsigned integers containing the corrected video frames, with time as axis 0.
+    """
     average_frame = np.mean(video_array, axis=0)
-    kernel_size = int(average_frame.shape[0]/8) * 2 + 1
+    kernel_size = int(average_frame.shape[0]/8) * 2 + 1 # Kernel size is one quarter the image width
     blur_frame = cv2.GaussianBlur(average_frame, (kernel_size,kernel_size), 0)
     target_brightness = np.mean(average_frame)
 
+    # loop through and correct each frame
     for i in range(video_array.shape[0]):
-            frame = video_array[i].astype(np.float32)
-            frame_brightness = np.mean(frame)
+        frame = video_array[i].astype(np.float32)
+        frame_brightness = np.mean(frame)
 
-            # Avoid division by zero
-            if frame_brightness != 0:
-                scaled_frame = frame * (target_brightness / frame_brightness)
-            else:
-                scaled_frame = frame  # Leave unchanged if average is zero
+        if frame_brightness != 0:
+            scaled_frame = frame * (target_brightness / frame_brightness) # scale each frame to have the same brightness as the average frame
+        else:
+            scaled_frame = frame  # Avoid division by zero
 
-            video_array[i] = (scaled_frame * target_brightness / blur_frame).astype(np.uint8)
+        # divide each frame by the blur of the average frame to correct vignetting or other large scale brightness variations
+        video_array[i] = (scaled_frame * target_brightness / blur_frame).astype(np.uint8)
 
     return video_array
 
 def subtract_average(video_array, average_start=0, average_end=-1, use_absolute_difference=True):
+    """
+    Subtracts the average frame from each frame in the video array.
+    This effectively only shows pixels which change in value over the course of the recording.
+    A range of frames can be specified to calculate the average frame, and an absolute difference can be used instead of clipped subtraction.
+    An ideal averaging range contains no stationary worms, as these will appear in negative space after subtraction.
+
+    Args:
+        video_array: 3D Numpy array containing the video frames, with time as axis 0.
+        average_start: Integer value for the start index of the frame range used to calculate the average frame.
+        average_end: Integer value for the end index of the frame range used to calculate the average frame.
+        use_absolute_difference: Boolean value for whether to use absolute difference or not.
+
+    Returns:
+        video_array: 3D Numpy array of 8 bit unsigned integers containing the subtracted video frames, with time as axis 0.
+    """
     if average_start == 0 and average_end == -1:
         average_frame = np.mean(video_array, axis=0)
     else:
@@ -30,33 +56,67 @@ def subtract_average(video_array, average_start=0, average_end=-1, use_absolute_
 
     target_brightness = np.mean(average_frame)
 
+    # loop through each frame to correct and subtract the average frame, leaving only motion visible
     for i in range(video_array.shape[0]):
-            frame = video_array[i].astype(np.float32)
-            frame_brightness = np.mean(frame)
+        frame = video_array[i].astype(np.float32)
+        frame_brightness = np.mean(frame) # brightness correction is performed in case vignetting correction was not used
 
-            # Avoid division by zero
-            if frame_brightness != 0:
-                scaled_frame = frame * (target_brightness / frame_brightness)
-            else:
-                scaled_frame = frame  # Leave unchanged if average is zero
+        if frame_brightness != 0:
+            scaled_frame = frame * (target_brightness / frame_brightness) # scale each frame to have the same brightness as the average frame
+        else:
+            scaled_frame = frame # Avoid division by zero
 
-            if use_absolute_difference:
-                video_array[i] = np.abs(scaled_frame - average_frame).astype(np.uint8)
-            else:
-                video_array[i] = np.clip(scaled_frame - average_frame, 0, None).astype(np.uint8)
+        # perform subtraction
+        if use_absolute_difference:
+            video_array[i] = np.abs(scaled_frame - average_frame).astype(np.uint8)
+        else:
+            video_array[i] = np.clip(scaled_frame - average_frame, 0, None).astype(np.uint8)
 
     return video_array
 
 def create_track_array(average_subtracted_array, window):
+    """
+    Projects along the time axis of the average subtracted array within a local frame window of the specified size to create frames with trails.
+    This is the simplest implementation of trails, and does not support colormaps to encode time within frames.
+    
+    Args:
+        average_subtracted_array: 3D Numpy array containing the average subtracted video frames, with time as axis 0.
+        window: Integer value for the window size used to create trails.
+
+    Returns:
+        track_array: 3D Numpy array of 8 bit unsigned integers containing the track array, with time as axis 0.
+    """
     track_array = []
+
+    # loop through each frame and project from the windows starting at that frame
     for i in range(average_subtracted_array.shape[0]):
-        track_frame = np.max(average_subtracted_array[i:i+window,:,:], axis=0)
+        track_frame = np.max(average_subtracted_array[i:i+window,:,:], axis=0) # project with maximum function since a dark background is used
         track_array.append(track_frame)
 
     return np.stack(track_array, axis=0)
 
 def create_time_encoded_array(average_subtracted_array, colormap=np.array([[0,0,0]]), window=20, scale_factor=1, offset=0, light_background=True):
+    """
+    Projects along the time axis of the average subtracted array within a local frame window of the specified size to create frames with trails.
+    Shorter windows make intuitive speed by trail length easier to see and work better for scans of densely populated plates.
+    Longer windows are better for seeing behavioral movement patterns, as long as the plate isn't too densely populated.
+    This implementation supports colormaps to encode time within frames.
+    
+    Args:
+        average_subtracted_array: 3D Numpy array containing the average subtracted video frames, with time as axis 0.
+        colormap: Numpy array containing the colormap, applied to each trail frame.
+        window: Integer value for the window size used to create trails, shorter windows take less processing time.
+        scale_factor: Float value for the scale factor, higher values effectively increase contrast.
+        offset: Integer value for the offset, positive values brighten the image, negative values darken it and can counteract noise.
+        light_background: Boolean value for the light background.
+
+    Returns:
+        time_encoded_array: 3D Numpy array of 8 bit unsigned integers containing the time encoded array of frames with trails, with time as axis 0.
+    """
+    
     time_encoded_array = []
+
+    # loop through the average subtracted array and create time encoded frames for the windows starting at each frame
     for i in range(average_subtracted_array.shape[0]-window):
         time_encoded_frame = create_time_encoded_frame(average_subtracted_array, colormap=colormap, window=window, start_time=i, scale_factor=scale_factor, offset=offset, light_background=light_background)
         time_encoded_array.append(time_encoded_frame)
@@ -64,46 +124,91 @@ def create_time_encoded_array(average_subtracted_array, colormap=np.array([[0,0,
     return np.stack(time_encoded_array, axis=0)
 
 def create_time_encoded_frame(average_subtracted_array, colormap=np.array([[0,0,0]]), window=20, start_time=0, scale_factor=1, offset=0, light_background=True):
-    mono_array = average_subtracted_array[start_time:start_time+window,:,:]
+    """
+    Projects along the time axis of the average subtracted array within a local frame window of the specified size to create a single frame with trails.
+    This implementation supports colormaps to encode time within frames.
+    
+    Args:
+        average_subtracted_array: 3D Numpy array containing the average subtracted video frames, with time as axis 0.
+        colormap: Numpy array containing the colormap, applied to the trail frame with color values being applied in order scaled to the window size.
+        window: Integer value for the window size used to create trails, shorter windows take less processing time. This is the number of frames after start_time to include in the trail.
+        start_time: Integer value for the start time of the frame. The window starts at this frame and continues forward for the specified window size.
+        scale_factor: Float value for the scale factor, higher values effectively increase contrast.
+        offset: Integer value for the offset, positive values brighten the image, negative values darken it and can counteract noise.
+        light_background: Boolean value for the light background.
+
+    Returns:
+        time_encoded_frame: 2D Numpy array of 8 bit unsigned integers containing the time encoded frame with trails.
+    """
+    mono_array = average_subtracted_array[start_time:start_time+window,:,:] # take just the frames within the window to be projected down along the time axis
     colormapped_array = []
+
+    # loop through the window and create colormapped frames
     for t in range(window):
-        tmap = colormap[int(np.shape(colormap)[0]*t/window),:]
-        if light_background:
+        tmap = colormap[int(np.shape(colormap)[0]*t/window),:] # get the color for the current frame from the colormap
+        if light_background: # invert the color used for colormapping if using a light background since it's going to be inverted back later
             tmap = 255 - tmap
+
+        # set the color of each pixel based on the colormap and the brightness based on the corresponding average subtracted frame
         colormapped_frame = np.clip(cv2.merge([tmap[0]*mono_array[t]/255, tmap[1]*mono_array[t]/255, tmap[2]*mono_array[t]/255])*scale_factor + offset, 0, 255).astype(np.uint8)
-        if light_background:
+        if light_background: # invert the colormapped frame if a light background is desired
             colormapped_frame = 255 - colormapped_frame
         colormapped_array.append(colormapped_frame)
 
     if light_background:
-        return np.min(colormapped_array, axis=0)
+        return np.min(colormapped_array, axis=0) # minimum pixel value is used for projections if we want dark tracks on a light background
     else:
-        return np.max(colormapped_array, axis=0)
+        return np.max(colormapped_array, axis=0) # maximum pixel value is used for projections if we want light tracks on a dark background
 
 def add_timestamp(video_array, black_background = True, font_scale=1, font_thickness=1, seconds_per_frame=1):
+    """
+    Adds a timestamp to each frame of the video array.
+    This should be done last in the processing pipeline.
+    
+    Args:
+        video_array: 3D Numpy array containing the video frames, with time as axis 0.
+        black_background: Boolean value for whether a black background is used.
+        font_scale: Float value for the font scale.
+        font_thickness: Integer value for the font thickness.
+        seconds_per_frame: Float value for the frame time in seconds. This is the captured frame time, not the desired export frame time.
+    
+    Returns:
+        video_array: 3D Numpy array of 8 bit unsigned integers containing the video frames with timestamps, with time as axis 0.
+    """
     num_frames, height, width = video_array.shape
     font = cv2.FONT_HERSHEY_SIMPLEX
-    position = (10, height-10)
+    position = (10, height-10) # Bottom left corner
     if black_background:
         color = 255 # 255 for white text, 0 for black text
     else:
         color = 0
 
+    # loop through and add timestamp to each frame
     for i in range(num_frames):
         total_seconds = int(i*seconds_per_frame)
-        timestamp = f"{(total_seconds // 60):02d}:{(total_seconds % 60):02d}"
-        color_frame = cv2.cvtColor(video_array[i].copy(), cv2.COLOR_GRAY2BGR)
+        timestamp = f"{(total_seconds // 60):02d}:{(total_seconds % 60):02d}" # Timestamp format mm:ss
+        color_frame = cv2.cvtColor(video_array[i].copy(), cv2.COLOR_GRAY2BGR) # the cv2.putText function requires a color image
         cv2.putText(color_frame, timestamp, position, font, font_scale, (color, color, color), font_thickness)
-        video_array[i] = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
+        video_array[i] = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY) # convert back to greyscale
 
     return video_array
 
 def normalize_array(video_array):
-    scale_factor = 255/np.max(video_array)
+    """
+    Normalizes the video array to a range of 0-255.
+    This is not necessary for properly exposed recordings, but can be useful for high bit depth or underexposed recordings.
+    
+    Args:
+        video_array: 3D Numpy array containing the video frames, with time as axis 0.
+
+    Returns:
+        normalized_array: 3D Numpy array of 8 bit unsigned integers containing the normalized video frames, with time as axis 0.
+    """
+    scale_factor = 255/np.max(video_array) # set one scale factor for all frames to avoid flickering
     normalized_array = np.empty_like(video_array, dtype=np.uint8)
 
     for i in range(video_array.shape[0]):
-        frame = video_array[i].astype(np.float32)
+        frame = video_array[i].astype(np.float32) # use 32 bit float while scaling
         scaled_frame = frame * scale_factor
 
         normalized_array[i] = scaled_frame.astype(np.uint8)
