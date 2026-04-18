6d4d9b0f46e20b32c36dda1e65522dd89b88e0f3
virgil <c.dante.ashih@gmail.com>
2026-04-18 19:09:34 -0400

    Version push.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wormtrails',
-    version='1.4.3',
+    version='1.4.4',
     author='Christopher Dante Ashih',
     description='A tool for generating track figures from video recordings of C. elegans',
     long_description=open('README.md').read(),
